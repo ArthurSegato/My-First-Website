5935a167322c23daf623497d9a300fdb526fa54a
Arthur Segato <19919287+ArthurSegato@users.noreply.github.com>
2019-12-31 00:03:32 -0300

    some little optimization

--- _script/script.js
+++ _script/script.js
@@ -1,9 +1,7 @@
 var open = false;
 var openAbout = false;
-
 function openNav() {
     var windowWidth = window.innerWidth;
-
     if (open == false) {
         $(".icon").toggleClass("close");
         if (windowWidth >= 1000) {
@@ -35,7 +33,6 @@ function openNav() {
         }
     }
 }
-
 function about() {
     document.getElementById("sideNav").style.width = "100%";
     document.getElementById("list").style.display = 'none';
