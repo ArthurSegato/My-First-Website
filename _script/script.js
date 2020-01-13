f38c7312dc76d7e5f080837394ab51b5558feb91
Arthur Segato <19919287+ArthurSegato@users.noreply.github.com>
2020-01-13 20:09:02 -0300

    add nekoware project

--- _script/script.js
+++ _script/script.js
@@ -1,8 +1,9 @@
-var open = false;
-var openAbout = false;
+var openHalf = false;
+var openFull = false;
+
 function openNav() {
     var windowWidth = window.innerWidth;
-    if (open == false) {
+    if (openHalf == false) {
         $(".icon").toggleClass("close");
         if (windowWidth >= 1000) {
             document.getElementById("sideNav").style.width = "25%";
@@ -11,31 +12,40 @@ function openNav() {
             document.getElementById("sideNav").style.width = "50%";
         }
         document.getElementById("fade").style.opacity = "0.65";
-        open = true;
+        openHalf = true;
     }
     else {
-        if (openAbout == true) {
+        if (openFull == true) {
             document.getElementById("list").style.display = 'block';
             document.getElementById("about").style.display = 'none';
+            document.getElementById("neko").style.display = 'none';
             if (windowWidth >= 1000) {
                 document.getElementById("sideNav").style.width = "25%";
             }
             else {
                 document.getElementById("sideNav").style.width = "50%";
             }
-            openAbout = false;
+            openFull = false;
         }
         else {
             $(".icon").toggleClass("close");
             document.getElementById("sideNav").style.width = "0";
             document.getElementById("fade").style.opacity = "0.5";
-            open = false;
+            openHalf = false;
         }
     }
 }
+
 function about() {
     document.getElementById("sideNav").style.width = "100%";
     document.getElementById("list").style.display = 'none';
     document.getElementById("about").style.display = 'block';
-    openAbout = true;
+    openFull = true;
+}
+
+function neko() {
+    document.getElementById("sideNav").style.width = "100%";
+    document.getElementById("list").style.display = 'none';
+    document.getElementById("neko").style.display = 'block';
+    openFull = true;
 }
